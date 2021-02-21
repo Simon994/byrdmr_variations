5622832213684b9371c425f15b5060db0e38da1b
SimonN <65023744+Simon994@users.noreply.github.com>
2021-02-21 09:12:53 +0000

    Add snapshot test for Contact component

---
+++ src/components/__tests__/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import renderer from 'react-test-renderer'
+import Contact from '../Contact'
+
+describe('The Contact component', () => {
+  it('renders as expected', () => {
+    const tree = renderer.create(
+      <BrowserRouter>
+        <Contact/>
+      </BrowserRouter>
+      ).toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+})
