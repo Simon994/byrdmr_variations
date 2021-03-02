b7492a7af26be54538e2aa3555220e1c5c98d6ad
SimonN <65023744+Simon994@users.noreply.github.com>
2021-03-02 10:47:08 +0000

    Split form from Contact component into seperate component and update to use hooks, via addition of a CustomHooks component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,97 +4,23 @@ import './Contact.css'
 import fbLogo from '../styles/assets/fb-logo.png'
 import instaLogo from '../styles/assets/insta-logo.png'
 import twitterLogo from '../styles/assets/twitter-logo.png'
-import buttonArrow from '../styles/assets/button-arrow.png'
 import DropdownMenu from './common/DropdownMenu'
-import { postMessage } from '../../src/lib/api'
+import Form from './Form'
 
-class Contact extends Component {
-
-  state = {
-    formData: {
-      fullName: '',
-      email: '',
-      message: ''
-    },
-    formError: false
-  }
-
-  handleChange = (e) => {
-    const formData = {
-      ...this.state.formData,
-      [e.target.name]: e.target.value
-    }
-
-    this.setState({
-      formData
-    })
-  }
-
-  handleSubmit = async (e) => {
-    e.preventDefault()
-    try {
-      await postMessage(this.state.formData)
-
-      this.setState({
-        formData: {
-          fullName: '',
-          email: '',
-          message: ''
-        }
-      })
-
-    } catch (err) {
-      this.setState({ formError: true })
-    }
-  }
-
-
-  render () {
-    const {fullName, email, message} = this.state.formData
-
-    return (
-      <div className='contact-container'>
-        <div className='social-container'>
-          <img src={fbLogo} className='social-logo' alt='facebook'/>
-          <img src={instaLogo} className='social-logo' alt='instagram'/>
-          <img src={twitterLogo} className='social-logo' alt='twitter'/>
-        </div>
-        <DropdownMenu/>
-        <div className='form-container'>
-          <form onSubmit={this.handleSubmit}>
-            <label className='form-label'>FULL NAME</label>
-            <input placeholder='Enter your full name'
-              onChange={this.handleChange}
-              value={fullName}
-              name='fullName'
-              className='input-regular'
-            />
-            <label className='form-label'>EMAIL</label>
-            <input placeholder='Enter your email'
-              onChange={this.handleChange}
-              value={email}
-              name='email'
-              className='input-regular'
-            />
-            <label className='form-label'>MESSAGE</label>
-            <textarea placeholder='What are your plans?'
-              onChange={this.handleChange}
-              value={message}
-              name='message'
-              className='input-large'
-            />
-            <button type='submit'>
-              <div>
-                Send
-                Message
-              </div>
-              <img src={buttonArrow} className='arrow' alt='arrow'/>
-            </button>
-          </form>
-        </div>
+const Contact = () => {
+  return (
+    <div className='contact-container'>
+      <div className='social-container'>
+        <img src={fbLogo} className='social-logo' alt='facebook'/>
+        <img src={instaLogo} className='social-logo' alt='instagram'/>
+        <img src={twitterLogo} className='social-logo' alt='twitter'/>
       </div>
-    )
-  }
+      <DropdownMenu/>
+      <div className='form-container'>
+        <Form/>
+      </div>
+    </div>
+  )
 }
 
 export default Contact
